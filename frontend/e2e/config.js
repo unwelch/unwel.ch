e716f67668aff22076f95921a90d1c15e675bcbb
David Sancho <dsnxmoreno@gmail.com>
2018-11-12 22:26:31 +0100

    refactor: Move e2e to the root

--- frontend/e2e/config.js
+++
@@ -1,1 +0,0 @@
-export const HOST = 'http://localhost:9000'
--- frontend/e2e/utils.js
+++
@@ -1,5 +0,0 @@
-import { Selector, ClientFunction } from 'testcafe'
-
-export const getLocation = ClientFunction(() => document.location.pathname)
-export const dataQaSelector = dataQa => `[data-qa="${dataQa}"]`
-export const dataQaExists = dataQa => Selector(dataQaSelector(dataQa)).exists
