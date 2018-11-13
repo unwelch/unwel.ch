d1e30ac224e0a60f7bb0df7c9697728fd461396a
David Sancho <dsnxmoreno@gmail.com>
2018-11-13 09:06:12 +0100

    ci: Add e2e folder #shame

---
+++ e2e/anonymous-create.js
@@ -0,0 +1,28 @@
+import { getLocation, dataQaSelector, dataQaExists } from './utils'
+import { HOST } from './config'
+
+fixture`Anonymous login by creation`.page`${HOST}`
+
+test('I can create a bet and login anonymously', async t => {
+  await t.click(dataQaSelector('make-bet-button'))
+
+  await t
+    .expect(await getLocation())
+    .eql(`/bets/new`, 'redirects to new bet page')
+
+  await t.typeText(dataQaSelector('bet-input-statement'), 'something')
+  await t.typeText(dataQaSelector('bet-input-quantity'), '1 coffee')
+
+  await t.click(dataQaSelector('create-bet-button'))
+
+  await t
+    .expect(await getLocation())
+    .eql(`/anonymous-login`, 'redirects to anonymous login')
+
+  await t.typeText(dataQaSelector('anonymous-login-input'), 'Gerard')
+  await t.click(dataQaSelector('anonymous-login-confirm'))
+
+  await t.expect(await getLocation()).eql(`/bets`, 'redirects to bet page')
+
+  await t.expect(dataQaExists('bet-list-item')).ok()
+})
