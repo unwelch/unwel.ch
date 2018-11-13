a74e4103e82669d9090ed607ea944d228f03d132
David Sancho <dsnxmoreno@gmail.com>
2018-11-13 12:40:56 +0100

    ci(travis): Merge all in one job

---
+++ backend/wait-for-db.js
@@ -0,0 +1,23 @@
+import pg from 'pg'
+
+require('dotenv').config()
+
+const main = async () => {
+  const client = new pg.Client({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    database: 'postgres',
+    port: process.env.DB_PORT
+  })
+
+  try {
+    await client.connect()
+    process.exit(0)
+  } catch (err) {}
+}
+
+setInterval(() => {
+  main()
+  console.log('Waiting for DB to be ready...')
+}, 100)
