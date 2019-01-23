dcca9292e77e6495d0ff3a29a95a99d03445e9a0
Gerard Abelló <gerardabello@gmail.com>
2019-01-23 18:14:47 +0100

    fix: Modal animation

--- frontend/components/modal.js
+++ frontend/components/modal.js
@@ -1,27 +1,7 @@
 import React from 'react'
-import styled, { keyframes } from 'styled-components'
+import styled from 'styled-components'
 
 import { colors } from 'components/variables'
-
-const appear = keyframes`
-  0% {
-    transform: translateY(20px);
-  }
-
-  100% {
-    transform: translateY(0px);
-  }
-`
-
-const disolve = keyframes`
-  0% {
-    opacity: 0;
-  }
-
-  100% {
-    opacity: 1;
-  }
-`
 
 const Root = styled.div`
   background: rgba(100, 100, 100, 0.5);
@@ -34,28 +14,44 @@ const Root = styled.div`
   align-items: center;
   justify-content: center;
   z-index: 1000;
-  animation: ${disolve} 1s ease;
+
   opacity: ${props => (props.isOpen ? '1' : '0')};
-  visibility: ${props => (props.isOpen ? 'visibile' : 'hidden')};
+  pointer-events: ${props => (props.isOpen ? 'auto' : 'none')};
+  transition: all 1s cubic-bezier(0.19, 1, 0.22, 1)
+    ${props => (props.isOpen ? '0s' : '0.2s')};
 `
 
 const ModalWrapper = styled.div`
   padding: 16px;
   margin: 16px;
+  background: ${colors.background};
+
+  border-radius: 6px;
+  box-shadow: 0px 4px 80px rgba(0, 0, 0, 0.35);
+
+  opacity: ${props => (props.isOpen ? '1' : '0')};
+  transform: translateY(${props => (props.isOpen ? '0' : '20')}px);
+  pointer-events: ${props => (props.isOpen ? 'auto' : 'none')};
+  transition: all 1s cubic-bezier(0.19, 1, 0.22, 1)
+    ${props => (props.isOpen ? '0.2s' : '0s')};
+`
+
+const ChildrenWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background: ${colors.background};
 
-  animation: ${appear} 1s ease;
-  border-radius: 6px;
-  box-shadow: 0px 4px 80px rgba(0, 0, 0, 0.35);
+  transform: translateY(${props => (props.isOpen ? '0' : '-40')}px);
+  transition: all 1s cubic-bezier(0.19, 1, 0.22, 1)
+    ${props => (props.isOpen ? '0.2s' : '0s')};
 `
 
 const Modal = ({ children, onClose, isOpen }) => (
   <Root onClick={onClose} isOpen={isOpen}>
-    <ModalWrapper>{children}</ModalWrapper>
+    <ModalWrapper isOpen={isOpen}>
+      <ChildrenWrapper isOpen={isOpen}>{children}</ChildrenWrapper>
+    </ModalWrapper>
   </Root>
 )
 
