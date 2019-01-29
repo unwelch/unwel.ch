0be2d146e148a1710c1206422068f95938335fcd
Gerard Abelló <gerardabello@gmail.com>
2019-01-29 18:17:52 +0100

    remove devtool

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -6,7 +6,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest')
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 module.exports = {
-  devtool: 'source-map',
   entry: {
     bundle: ['@babel/polyfill', './src/index.js']
   },
