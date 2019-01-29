5e51575f22a38c0b5db02f3de55a814cc0d2e8d3
Gerard Abelló <gerardabello@gmail.com>
2019-01-29 11:49:32 +0100

    Revert "try fix docker"
    This reverts commit 895e638a2487500d6012b8114e87ed650e2824f5.

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,7 @@
 docker login -u="$DOCKERHUB_USERNAME" -p="$DOCKERHUB_PASSWORD"
-cd ./frontend
-docker build -t frontend .
+docker build -t frontend ./frontend
 docker tag frontend unwelch/frontend
-cd ..
-cd ./backend
-docker build -t backend .
+docker build -t backend ./backend
 docker tag backend unwelch/backend
-cd ..
 docker push unwelch/frontend
 docker push unwelch/backend
