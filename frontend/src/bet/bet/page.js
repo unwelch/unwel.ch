dd78cb9cd21edce0a5b123e7a4f0b97b5015b418
David Sancho <dsnxmoreno@gmail.com>
2019-02-01 12:42:26 +0100

    refactor: Abstract actions from bet/page

--- frontend/src/bet/bet/page.js
+++ frontend/src/bet/bet/page.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import copy from 'copy-text-to-clipboard'
 import { graphql } from 'react-apollo'
 import { compose } from 'ramda'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import ShareIcon from 'react-feather/dist/icons/share'
-import TrashIcon from 'react-feather/dist/icons/trash-2'
 
 import { saveTempAccept } from '../services'
 import {
@@ -19,64 +16,25 @@ import { BET_LIST } from '../bet-list/queries'
 import { showAnnounce } from '../../announce/actions'
 import { betStatuses, getBetStatus } from '../bet-status'
 import { getIntroText, getAccepterText } from '../phrase-generator'
-import FinishBet from './finish-bet'
+import Actions from './actions'
 import { trackEvent, events } from '../../tracking'
 import withNavigation from '../../navigation/withNavigate'
 
 import { TranslatorConsumer } from '../../translations'
 
-import Button from 'components/button'
 import DefaultContainer from 'components/default-container'
 import Distribute from 'components/distribute'
 import Avatar from 'components/avatar'
 import Text from 'components/text'
 import Spacer from 'components/spacer'
 import Placeholder from 'components/placeholder'
-import { colors } from 'components/variables'
-
-const BET_PAGE = `${window.location.origin}/bet`
-
-const webShareEnabled = () => window.navigator.share
 
 class BetPage extends Component {
-  constructor (props) {
-    super(props)
-
-    this.acceptBet = this.acceptBet.bind(this)
-    this.deleteBet = this.deleteBet.bind(this)
-    this.shareLink = this.shareLink.bind(this)
-    this.chooseWon = this.chooseWon.bind(this)
-    this.chooseLost = this.chooseLost.bind(this)
-  }
-
   componentDidMount () {
     trackEvent(events.pageLoaded, { page: 'bet' })
   }
 
-  shareLink (title, text, url) {
-    return () => {
-      if (webShareEnabled()) {
-        navigator
-          .share({
-            title,
-            text,
-            url
-          })
-          .then(() => console.log('Successful share'))
-          .catch(error => console.log('Error sharing', error))
-      } else {
-        this.props.showAnnounce('announce.bet-url-copied')
-        copy(url)
-      }
-
-      trackEvent(events.betLinkCopied, {
-        betId: this.props.betId,
-        isWebAPIShareSupported: !!webShareEnabled()
-      })
-    }
-  }
-
-  acceptBet () {
+  acceptBet = () => {
     const { currentUser } = this.props.data
     trackEvent(events.acceptBetClick, { betId: this.props.betId })
 
@@ -90,17 +48,17 @@ class BetPage extends Component {
     }
   }
 
-  chooseWon () {
+  chooseWon = () => {
     this.props.chooseWinner(this.props.betId, true)
     this.props.showAnnounce('announce.bet-marked-won')
   }
 
-  chooseLost () {
+  chooseLost = () => {
     this.props.chooseWinner(this.props.betId, false)
     this.props.showAnnounce('announce.bet-marked-lost')
   }
 
-  deleteBet () {
+  deleteBet = () => {
     if (window.confirm('Are you sure you want to delete this bet?')) {
       this.props.deleteBet(this.props.betId)
       trackEvent(events.betDeleted, { betId: this.props.betId })
@@ -108,88 +66,8 @@ class BetPage extends Component {
     }
   }
 
-  renderActions (t) {
-    const { currentUser, bet } = this.props.data
-
-    const betStatus = getBetStatus(bet, currentUser)
-
-    switch (betStatus) {
-      case betStatuses.WAITING_FOR_OPONENT:
-        return (
-          <Distribute vertical space={2}>
-            <Button
-              type='level2'
-              onClick={this.shareLink(
-                t('copy-message.body'),
-                t('copy-message.title'),
-                `${BET_PAGE}/${this.props.betId}`
-              )}
-              icon={<ShareIcon />}
-            >
-              {t(
-                webShareEnabled() ? 'bet-actions.share' : 'bet-actions.copy-url'
-              )}
-            </Button>
-            <Button
-              type='warning'
-              icon={<TrashIcon />}
-              onClick={this.deleteBet}
-            >
-              {t('bet-actions.delete')}
-            </Button>
-          </Distribute>
-        )
-      case betStatuses.AVAILABLE_BET:
-        return (
-          <Button
-            type='level2'
-            onClick={this.acceptBet}
-            dataQa='accept-bet-button'
-          >
-            {t('bet-actions.accept-bet')}
-          </Button>
-        )
-      case betStatuses.WAITING_FOR_USER_RESPONSE:
-        return (
-          <FinishBet
-            bet={bet}
-            currentUser={currentUser}
-            chooseWon={this.chooseWon}
-            chooseLost={this.chooseLost}
-          />
-        )
-      case betStatuses.WAITING_FOR_OPONENT_RESPONSE:
-        const opponent = bet.user.id === currentUser.id ? bet.user2 : bet.user
-
-        return (
-          <Text dimmed size='size2'>
-            {t('bet-status.waiting-for-opponent-response', {
-              opponent: opponent.name
-            })}
-          </Text>
-        )
-      case betStatuses.LOST:
-        return (
-          <Text fontWeight='black' size='size2'>
-            {t('bet-status.lost.long')}
-          </Text>
-        )
-      case betStatuses.WON:
-        return (
-          <Text fontWeight='black' color={colors.primary} size='size2'>
-            {t('bet-status.won.long')}
-          </Text>
-        )
-      case betStatuses.DISPUTED:
-        return (
-          <Text fontWeight='black' color={colors.error} size='size2'>
-            {t('bet-status.dispute.long')}
-          </Text>
-        )
-    }
-  }
-
   render () {
+    const { showAnnounce } = this.props
     const { bet, currentUser, loading } = this.props.data
 
     if (loading) {
@@ -280,7 +158,18 @@ class BetPage extends Component {
               <Link to={`/bet/${bet.id}/edit`} />
             ) : null}
 
-            <Spacer bottom={20}>{this.renderActions(t)}</Spacer>
+            <Spacer bottom={20}>
+              <Actions
+                t={t}
+                bet={bet}
+                currentUser={currentUser}
+                showAnnounce={showAnnounce}
+                acceptBet={this.acceptBet}
+                deleteBet={this.deleteBet}
+                chooseLost={this.chooseLost}
+                chooseWon={this.chooseWon}
+              />
+            </Spacer>
           </DefaultContainer>
         )}
       </TranslatorConsumer>
